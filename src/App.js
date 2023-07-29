af5ef449217bb8609ab4197f92c1f0e37c175052
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-29 13:21:27 +0600

    login route add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import RegistrationPage from './pages/Users/RegistrationPage'
+import LoginPage from './pages/Users/LoginPage'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
      <BrowserRouter>
      <Routes>
         <Route path="/registration" element={<RegistrationPage />}/>
+        <Route path="/login" element={<LoginPage />} />
      </Routes>
      </BrowserRouter>
     </>
