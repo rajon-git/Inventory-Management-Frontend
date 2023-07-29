21f0de21638ef56186627bd18efbd1d2d7a4d248
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-29 13:34:17 +0600

    profile store add

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import settingsReducer from "../slice/settingsSlice";
+import profileReducer from "../slice/profileSlice";
 
 export default configureStore({
     reducer: {  
         settings: settingsReducer,
+        profile: profileReducer,
     },
 });
