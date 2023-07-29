600d0bdd86c550a155fe1ff34fde60f4787a32b9
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-29 13:08:07 +0600

    store add

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import settingsReducer from "../slice/settingsSlice";
 
 export default configureStore({
     reducer: {  
-         
+        settings: settingsReducer,
     },
 });
