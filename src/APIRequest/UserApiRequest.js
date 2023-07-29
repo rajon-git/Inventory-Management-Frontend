d637f80f6f8a3cc969ae551ae2dabd93fbb45805
Ecogreen Technology <ecogreenproglobal1@gmail.com>
2023-07-29 13:05:04 +0600

    user registration api add

---
+++ src/APIRequest/UserApiRequest.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { ErrorToast, SuccessToast } from "../helper/FormHelper";
+import {
+  getToken,
+  setEmail,
+  setOtp,
+  setToken,
+  setUserDetails,
+} from "../helper/SessionHelper";
+import { BaseURL } from "../helper/config";
+import { SetProfile } from "../redux/slice/profileSlice";
+import { HideLoader, ShowLoader } from "../redux/slice/settingsSlice";
+import store from "../redux/store/store";
+const axiosConfig = { headers: { token: getToken() } };
+
+// Registration Request
+export async function RegistrationRequest(
+    UserEmail,
+    firstName,
+    lastName,
+    mobile,
+    password,
+    photo
+  ) {
+    try {
+      store.dispatch(ShowLoader());
+      let URL = `${BaseURL}/registration`;
+  
+      let PostBody = {
+        UserEmail,
+        firstName,
+        lastName,
+        mobile,
+        password,
+        photo,
+      };
+  
+      let res = await axios.post(URL, PostBody);
+      store.dispatch(HideLoader());
+  
+      if (res.status === 201) {
+        if (res.data["status"] === "fail") {
+          ErrorToast("Email Already Exist");
+        } else {
+          SuccessToast("Registration Success");
+          return true;
+        }
+      } else {
+        ErrorToast("Something Went Wrong");
+        return false;
+      }
+    } catch (e) {
+      store.dispatch(HideLoader());
+      ErrorToast("Something Went Wrong");
+      return false;
+    }
+  }
